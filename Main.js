25fda5be513089d5e76b3092fc5f338820b7fba0
Wacu Mbugua <wmbugua@CIM.CO.KE>
2019-12-24 00:00:11 +0300

    JS commit

---
+++ Main.js
@@ -0,0 +1,56 @@
+console.log('hellow node!');
+
+alert('be happy, dont worry!');
+
+
+
+
+function getAverage (a,b) {
+
+    var average = (a + b) / 2;
+    console.log(average);
+}
+getAverage(7,122);
+
+
+  var names = ('ice', 'dice', 'spice');
+name.length
+const human = {
+    firstName: 'Wacu'
+    lastName: 'Mbugua',
+    age: 29,
+    sayName () {
+      console.log(`I am Wacu Mbugua`)
+    }
+  }
+  
+  human.sayName()
+
+  / Ugh, creating another human by writing a new object
+const Nicolas = {
+  firstName: 'Judy',
+  lastName: 'She',
+  age: 30,
+  sayName () {
+    console.log(`I am Judy She`)
+  }
+}
+
+function Human (firstName, lastName, age) {
+    // ???
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+  
+    this.sayName = function () {
+      console.log(`I am ${firstName} ${lastName}`)
+    }
+  }
+  //objects
+  varperson1={
+    name:"Janedoe"
+    };
+    
+    varperson2=newObject();
+    person2.name="Janedoe";
+    
